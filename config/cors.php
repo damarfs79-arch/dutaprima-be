84d3c4021fc9c59904257e10c4ab8656e7d99cac
damarfs <damarfs79@gmail.com>
2026-07-14 14:35:23 +0700

    Fix CORS for frontend domain

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,15 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+        'http://localhost:5173',
+        'http://localhost:4173',
+        'https://duta.smkprima.com',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
